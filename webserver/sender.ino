6c01fb93853bd044e681748e7e991b766d363693
uino <foo>
2015-06-14 11:28:15 +0200

    webserver

--- webserver/sender.ino
+++ webserver/sender.ino
@@ -7,71 +7,78 @@
 #include <SPI.h>
 #include <Ethernet.h>
 
+#define SERVER "www.test.fr"
+//==> sur la ligne ci dessus remplacer "test" par le bon nom
+
+
 // Enter a MAC address for your controller below.
 // Newer Ethernet shields have a MAC address printed on a sticker on the shield
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+
 // if you don't want to use DNS (and reduce your sketch size)
 // use the numeric IP instead of the name for the server:
 //IPAddress server(74,125,232,128);  // numeric IP for Google (no DNS)
-char server[] = "www.google.com";    // name address for Google (using DNS)
+char server[] = SERVER;    // name address for Google (using DNS)
 
 // Set the static IP address to use if the DHCP fails to assign
-IPAddress ip(192,168,0,177);
+// pas utilisé pour l'instant // IPAddress ip(192,168,0,177);
 
-// Initialize the Ethernet client library
-// with the IP address and port of the server 
-// that you want to connect to (port 80 is default for HTTP):
 EthernetClient client;
 
-void setup() {
- // Open serial communications and wait for port to open:
-  Serial.begin(9600);
-   while (!Serial) {
-    ; // wait for serial port to connect. Needed for Leonardo only
-  }
+
+
+
+void sendMessage(long message) {
 
   // start the Ethernet connection:
   if (Ethernet.begin(mac) == 0) {
     Serial.println("Failed to configure Ethernet using DHCP");
+    return;
+
     // no point in carrying on, so do nothing forevermore:
     // try to congifure using IP address instead of DHCP:
-    Ethernet.begin(mac, ip);
+    // ==> j'ai désactivé ça pour l'instant: Ethernet.begin(mac, ip);
   }
   // give the Ethernet shield a second to initialize:
   delay(1000);
   Serial.println("connecting...");
 
   // if you get a connection, report back via serial:
-  if (client.connect(server, 80)) {
+  if (client.connect(SERVER, 80)) {
     Serial.println("connected");
-    // Make a HTTP request:
-    client.println("GET /search?q=arduino HTTP/1.1");
-    client.println("Host: www.google.com");
+    client.print("GET /ino/index.php?log=");
+    client.print(message); // le message à envoyer (sans caractères spéciaux)
+    client.println(" HTTP/1.1");
+    client.print("Host: ");
+    client.println(SERVER);
     client.println("Connection: close");
     client.println();
   } 
   else {
     // kf you didn't get a connection to the server:
     Serial.println("connection failed");
+    return;
   }
+
+  Serial.println("disconnecting.");
+  client.stop(); 
+  // (!client.connected())
 }
+
+
+
+void setup() {
+  Serial.begin(9600);
+
+}
+
+long lastDate = 0;
 
 void loop()
 {
-  // if there are incoming bytes available 
-  // from the server, read them and print them:
-  if (client.available()) {
-    char c = client.read();
-    Serial.print(c);
-  }
-
-  // if the server's disconnected, stop the client:
-  if (!client.connected()) {
-    Serial.println();
-    Serial.println("disconnecting.");
-    client.stop();
-
-    // do nothing forevermore:
-    while(true);
+  long now = millis();
+  if (now - lastDate > 5000) { // message toutes les 5 secondes
+    lastDate = now;
+    sendMessage(now);
   }
 }
